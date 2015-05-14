3177650ac0a8c2f79880cfdfd91032d4e72a9cc2
krit <krit@adobe.com>
2015-05-14 15:43:49 +0530

    added steps to run ssl labs assertion changed the base url for sslyze

--- lib/gauntlt/attack_adapters/ssllabs.rb
+++ lib/gauntlt/attack_adapters/ssllabs.rb
@@ -1,7 +1,12 @@
 When /^I launch (?:a|an) "ssllabs-scan" attack with:$/ do |command|
+	# ssllabs output function prints the output even if the execution is successful
+  # all_output variable has the complete output of the command
   run_with_profile command
+  puts Constant.scan_output_flag['start']
+  puts  all_output.to_s + ':' + all_output.class.to_s
+  puts Constant.scan_output_flag['end']
 end
+
 When /^"ssllabs-scan" is installed$/ do
   ensure_cli_installed("ssllabs-scan")
 end
-
