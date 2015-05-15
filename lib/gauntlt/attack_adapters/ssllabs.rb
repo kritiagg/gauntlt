00b0a548d2bb16f6b86e0a12ad74cea8212092df
krit <krit@adobe.com>
2015-05-15 16:04:05 +0530

    modified ssl labs attack adapter to print the scan tool output

--- lib/gauntlt/attack_adapters/ssllabs.rb
+++ lib/gauntlt/attack_adapters/ssllabs.rb
@@ -2,9 +2,9 @@ When /^I launch (?:a|an) "ssllabs-scan" attack with:$/ do |command|
 	# ssllabs output function prints the output even if the execution is successful
   # all_output variable has the complete output of the command
   run_with_profile command
-  puts Constant.scan_output_flag['start']
+  puts "Raw Tool output:$$ start"
   puts  all_output.to_s + ':' + all_output.class.to_s
-  puts Constant.scan_output_flag['end']
+  puts "Raw Tool output:$$ end"
 end
 
 When /^"ssllabs-scan" is installed$/ do
